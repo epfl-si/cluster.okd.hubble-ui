4188da832d81ddfe17583d2417919c3603e8e434
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2024-05-13 10:37:21 +0200

    [feature] Minimalistic (read-only) iframe functionality
    - Provide  generic `src/iframe-api.ts`, containing an `onIframeMessage` implementation that dutiflly checks message origin (as per https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage#security_concerns ) and message `.kind`
    - Obtain 12-factored configuration from `/api/plugins/okd-epfl-hubble-ui/runtime/frontend-config.json` consisting of `{ iframeDomain, tokenReflectorURI }`
    - Create and embed the iframe from the URL in `iframeDomain`
    - Honor its `bearer-token-request` messages using an async call to the token reflector

--- src/components/HubbleUIDiv.tsx
+++ src/components/HubbleUIDiv.tsx
@@ -1,51 +1,70 @@
 import * as React from 'react';
+import { useRef,useEffect } from 'react';
 import Helmet from 'react-helmet';
 import {
   Page,
   PageSection,
-  Text,
-  TextContent,
   Title,
 } from '@patternfly/react-core';
+import useFetch from 'use-http';
+
 import './hubble-ui-div.css';
+import { onIframeMessage } from  '../iframe-api';
+
+type UseFetchError = ReturnType<typeof useFetch>['error'];
+
+function t(translatableString : string) { return translatableString; }
 
 export default function HubbleUIDiv() {
-  function t(translatableString : string) { return translatableString; }
+
+  const { data, error } = useFetch(
+    "/api/plugins/okd-epfl-hubble-ui/runtime/frontend-config.json",
+    {credentials: "include"});
 
   return (
     <>
       <Helmet>
-        <title data-test="example-page-title">{t('Hello, Plugin!')}</title>
+        <title data-test="example-page-title">{t('Hubble UI')}</title>
       </Helmet>
       <Page>
         <PageSection variant="light">
-          <Title headingLevel="h1">{t('Hello, Plugin!')}</Title>
+          <Title headingLevel="h1">{t('Hubble UI')}</Title>
         </PageSection>
-        <PageSection variant="light">
-          <TextContent>
-            <Text component="p">
-              <span className="okd-epfl-hubble-ui__nice">
-                {t('Nice!')}
-              </span>{' '}
-              {t('Your plugin is working.')}
-            </Text>
-            <Text component="p">
-              {t(
-                'This is a custom page contributed by the console plugin template. The extension that adds the page is declared in console-extensions.json in the project root along with the corresponding nav item. Update console-extensions.json to change or add extensions. Code references in console-extensions.json must have a corresponding property',
-              )}
-              <code>{t('exposedModules')}</code>{' '}
-              {t('in package.json mapping the reference to the module.')}
-            </Text>
-            <Text component="p">
-              {t('After cloning this project, replace references to')}{' '}
-              <code>{t('console-template-plugin')}</code>{' '}
-              {t(
-                'and other plugin metadata in package.json with values for your plugin.',
-              )}
-            </Text>
-          </TextContent>
-        </PageSection>
+        { data ? <HubbleUI {...data} /> : error ? <Error error={error}/>: <Loading/> }
       </Page>
     </>
   );
+}
+
+function HubbleUI({ iframeDomain, tokenReflectorURI}:
+  { iframeDomain : string, tokenReflectorURI : string }) {
+    const iframeRef = useRef<HTMLIFrameElement>(null);
+
+    useEffect(() => {
+      return onIframeMessage(iframeRef, "bearer-token-request",
+        async () => {
+          try {
+            const res = await fetch(tokenReflectorURI, {credentials: "include"});
+            const token = res.headers.get("X-Token");
+            iframeRef.current.contentWindow.postMessage(
+              { kind: "token",
+                token });
+          } catch (e) {
+            console.error("in onIframeMessage handler: ", e);
+          }
+        });
+    });
+
+    return <iframe ref={iframeRef} src={iframeDomain}></iframe>
+}
+
+function Error ({ error }: { error: UseFetchError }) {
+  return <>
+           <h4>{t("Error!")}</h4>
+           <pre>{JSON.stringify(error)}</pre>
+         </>;
+}
+
+function Loading () {
+  return <>⏳</>;
 }
---
+++ src/iframe-api.ts
@@ -0,0 +1,62 @@
+/**
+ * API for sending / receiving messages with the Hubble UI
+ *
+ * TODO: find a way to share the types with https://github.com/epfl-si/hubble-ui-epfl/blob/feature/mashable/src/iframe-api/index.ts
+ */
+
+interface KV {
+  key: string;
+  value: string;
+}
+
+export type IframeSentMessage = BearerToken;
+
+type BearerToken = {
+  kind: "token",
+  token: string
+}
+
+export type IframeReceivedMessage = BearerTokenRequest | EndpointCardClicked;
+
+type BearerTokenRequest = {
+  kind: "bearer-token-request"
+}
+
+type EndpointCardClicked = {
+  kind: "endpoint-card-clicked-⚙️",
+  namespace ?: string,
+  labels: Array<KV>
+}
+
+/**
+ * @return A void function that unregisters the handler when called.
+ */
+export function onIframeMessage<T extends IframeReceivedMessage['kind']> (
+  originIframeRef: { current: HTMLIFrameElement },
+  kind : T,
+  handler : (message : IframeReceivedMessage & { kind : T }) => void)
+: () => void {
+  const secureHandler = (event : MessageEvent) => {
+    // A number of browser plug-ins do `window.postMessage` from the
+    // window to itself; see
+    // https://github.com/facebook/react/issues/27529#issuecomment-1766536750
+    if (event.source === window) return;
+
+    if (! originIframeRef.current) {
+      console.log("Too soon?");
+      return;
+    }
+
+    if (originIframeRef.current.contentWindow !== event.source) {
+      console.log("OMG H4X!!");
+      return;
+    }
+
+    if (event.data?.kind !== kind) return;
+
+    handler(event.data);
+  }
+
+  window.addEventListener("message", secureHandler);
+  return () => window.removeEventListener("message", secureHandler);
+}
